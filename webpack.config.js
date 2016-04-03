aa3e825f1ab6c7bbd0527df8928ff37ad21f05df
nikoloza <nika.tomadze@gmail.com>
2016-04-03 23:23:40 +0200

    first stage

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,33 @@
 // var path = require('path')
 var autoprefixer = require('autoprefixer')
 var precss = require('precss')
+var cssimport = require('postcss-import')
 var url = require('postcss-url')
+var cssnext = require('postcss-cssnext')
 
 module.exports = {
   entry: './app.js',
+  template: './index.html',
   output: {
-    path: require('path').resolve('build'),
-    publicPath: '/',
+    path: __dirname + "/dist",
     filename: 'bundle.js'
   },
   module: {
-    loaders: [
-      {
-        test: /\.css$/,
-        loader: 'style-loader!css-loader?modules&importLoaders=1!postcss-loader'
-      }
-    ]
+    loaders: [{
+      test: /\.css$/,
+      // loader: 'style-loader!css-loader?modules&importLoaders=1!postcss-loader'
+      loader: 'style-loader!css-loader!postcss-loader'
+    }]
   },
   postcss: function (webpack) {
     return [
       autoprefixer,
       precss,
-      postcssImport({
+      cssimport({
         addDependencyTo: webpack
       }),
-      require('postcss-cssnext')()
+      cssnext()
     ]
   },
   plugins: []
-};
+}
